2702671a16574c12c7852590c97160373cc1d12d
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2017-04-03 07:02:24 +0200

    alignment possible just like for images

--- syntax.php
+++ syntax.php
@@ -10,6 +10,10 @@
 if (!defined('DOKU_INC')) die();
 
 class syntax_plugin_a2s extends DokuWiki_Syntax_Plugin {
+    protected static $cssAlign=array(
+        'none' => 'media', 'left' => 'medialeft',
+        'right' => 'mediaright', 'center' => 'mediacenter'
+    );
     /**
      * return some info.
      * Why did I copy this function here ? old DW compat ???
@@ -87,18 +91,30 @@ class syntax_plugin_a2s extends DokuWiki_Syntax_Plugin {
      * @return bool If rendering was successful.
      */
     public function render($mode, Doku_Renderer $renderer, $data) {
-        require_once(dirname(__FILE__).'/a2s.php');
         if($mode != 'xhtml') return false;
         $state='';
-        list($state, $match) = $data;
+        list($state, $passed_in) = $data;
+
         switch ($state) {
         case DOKU_LEXER_ENTER :
-            $a=2;
-            // TODO: check $match left and right spaces, in order to align.
+            $spaces=array();
+            preg_match( '/<( *)a2s( *)>/', $passed_in, $spaces );
+            $left=strlen($spaces[1]);
+            $right=strlen($spaces[2]);
+            $align='none';
+            if( ($right + $left) > 0 ) {
+                if( $right > $left )
+                    $align='left';
+                elseif( $left > $right)
+                    $align='right';
+                else
+                    $align='center';
+            }
+            $align=self::$cssAlign[$align];
+            $renderer->doc .= "<svg class=\"{$align}\" ";
         break;
         case DOKU_LEXER_UNMATCHED :
-            $scale = array(9, 16);
-            $o = new \org\dh0\a2s\ASCIIToSVG($match);
+            $o = new \org\dh0\a2s\ASCIIToSVG($passed_in);
             $o->setDimensionScale(9, 16);
             $o->parseGrid();
             $renderer->doc .= $o->render();
